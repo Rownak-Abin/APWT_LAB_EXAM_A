b5683ad82572598d153e547efaf87dc34eca16bc
Rownak-Abin <62964382+Rownak-Abin@users.noreply.github.com>
2020-12-19 13:34:55 +0600

    Add files via upload

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+</head>
+<body>
+
+	<h1>Welcome Admin</h1>
+	<a href="/create">Create New User</a> |
+	<a href="/userlist">View User List</a> |
+	<a href="/logout">logout</a>
+
+	<br>
+	<br>
+	
+</body>
+</html>
---
+++ resources/views/employee/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Employee Home Page</title>
+</head>
+<body>
+
+	<h1>Welcome Employee</h1>
+	<a href="/createProduct">Create New Product</a> |
+	<a href="/Productlist">View Product List</a> |
+	<a href="/logout">logout</a>
+
+	<br>
+	<br>
+	
+</body>
+</html>
