2fc5f11934623b05da58b312c78ad1ae198a7546
Rownak-Abin <62964382+Rownak-Abin@users.noreply.github.com>
2020-12-19 16:27:33 +0600

    product complete

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -6,8 +6,8 @@
 <body>
 
 	<h1>Welcome Employee</h1>
-	<a href="/createProduct">Create New Product</a> |
-	<a href="/Productlist">View Product List</a> |
+	<a href="/ProductCreate">Create New Product</a> |
+	<a href="/ProductList">View Product List</a> |
 	<a href="/logout">logout</a>
 
 	<br>
